f4626234ce859db329a51f2be18345f0fa86d66b
ZelalDev <zelall2222@gmail.com>
2025-12-06 14:22:19 +0300

    code created

--- Strings/main.swift
+++ Strings/main.swift
@@ -7,5 +7,11 @@
 
 import Foundation
 
-print("Hello, World!")
+//create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
 
+let strings = ["BMW", "Mercedes", "Toyota", "BMW", "Fiat", "Honda", "Toyota", "Ford", "BMW"]
+
+print(strings.count)
+let uniqeStrings = Set(strings)
+
+print(uniqeStrings.count)
